65f99060d60d0300f563eb49f17e21a3aa445148
Marco Antônio Queiroz <marco.aq7@gmail.com>
2022-11-22 11:06:56 -0300

    Update shell init

--- shell/init.sh
+++ shell/init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Atualiza servidor
-git stash
-git pull --ff-only
-
-# Atualiza app
-npm update
 
 # App
 while true; do
+    # Atualiza servidor
+    git stash
+    git pull --ff-only
+
+    # Atualiza app
+    npm update
+
+    # Start...
     sleep 10
     npm run start
 done
