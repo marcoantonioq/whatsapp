989899ee18c6786415dd6fcb1fec398ad1bd66b3
Marco Antônio Queiroz <marco.aq7@gmail.com>
2023-01-04 21:26:25 -0300

    Update

--- src/Whatsapp/apiReboot.ts
+++ src/Whatsapp/apiReboot.ts
@@ -4,10 +4,10 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 app.on("message_create", async (msg) => {
-  const reboot = /^reboot$|^restart$/gi;
-  if (msg.body.match(reboot)) {
-    if (api.isToAPI(msg)) {
-      console.log("Reboot iniciado!!!!");
+  if (api.isToAPI(msg)) {
+    const reboot = /^reboot$|^restart$/gi;
+    if (msg.body.match(reboot)) {
+      api.sendToAPI("Reiniciando API....");
       const { err, stdout, stderr } = await exec("/sbin/reboot");
       if (err) console.log(`Erro shell exec: ${err.message}`);
       if (stderr) console.log(`Erro shell stderr: ${stderr}`);
