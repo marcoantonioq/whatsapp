b32f29529fd91adc986b41ff1d2a960993cb2404
Marco Antônio <marco.aq7@gmail.com>
2023-03-03 13:20:13 -0300

    Update

--- src/modules/template/html-to-image.ts
+++ src/modules/template/html-to-image.ts
@@ -20,6 +20,27 @@ const init = async (output: string, values: any) => {
   }
 };
 
-init("./out/image.png", { name: "Marco Antônio" }).then(() =>
+const content = `
+      <h1>REUNIÃO DE EVANGELIZAÇÃO</h1>
+      <h2>HOJE - 19H30</h2>
+      <span> Casa da Ir. Domingas - Papyrus</span>
+      <cite>
+        Deus é o nosso refúgio e nossa fortaleza, socorro bem presente nas
+        angústias. Portanto não temeremos, ainda que a terra se mude, e ainda
+        que os montes se transportem para o meio dos mares. Ainda que as águas
+        tumultuem e se turvem, ainda que os montes se abalem com o seu fragor.
+        <br />
+        - Salmo 46
+      </cite>
+      <small>
+        <b>Próximas reuniões:</b>
+        07/03 às 19h30 Ir. Cristina - Portal da Cerra </br>        
+        12/03 às 14h30 Ir. Ari - São Carlos </br>
+        14/03 às 19h30 Ir. Francisco - Papyrus </br>
+        21/03 às 19h30 Ir. Sebastião - Jardim das Acácias
+      </small>
+    `;
+
+init("./out/image.png", { name: "Marco Antônio", content: content }).then(() =>
   console.log("Imagem criada com sucesso!")
 );
